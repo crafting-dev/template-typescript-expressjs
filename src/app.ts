53aa7ae233d4653a32ad84aba031340beb1914b9
Alexander Maru <i@alexandermaru.com>
2021-11-05 19:51:34 -0400

    Enable CORS

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/ping', (req, res) => {
   const pong = {
