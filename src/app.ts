5bc7b708ae72548604e8ee165839c29011652799
Alexander Maru <i@alexandermaru.com>
2022-01-20 03:48:33 -0500

    update and cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get('/ping', (req, res) => {
   const pong = {
     ping: req.query.ping || 'To ping, or not to ping; that is the question.',
     received_at: new Date(),
-  }
+  };
 
-  res.json(pong)
-})
+  res.json(pong);
+});
 
-app.listen(PORT)
+app.listen(PORT);
